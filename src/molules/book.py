51136e7aebd451be7e475369750e49f3f16908d5
Natalia Voitenko <Natalia.voitenko28@gmail.com>
2024-08-14 22:23:42 +0000

    Реалізовано клас ContactBook

--- src/molules/book.py
+++ src/molules/book.py
@@ -1,36 +1,37 @@
 import json
 import os
+from collections import UserDict
 
-class AddressBook:
-    def __init__(self, filename='address_book.json'):
+class ContactBook(UserDict):
+    def __init__(self, filename='contacts.json'):
+        super().__init__()
         self.filename = filename
-        self.contacts = {}
-        if os.path.exists(self.filename):
-            self.load()
-        else:
-            self.save()
+        self.load()
 
-    def add_contact(self, name, address, phone):
-        self.contacts[name] = {'address': address, 'phone': phone}
+    def add_record(self, record):
+        self.data[record.name] = record.value
         self.save()
 
-    def remove_contact(self, name):
-        if name in self.contacts:
-            del self.contacts[name]
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
             self.save()
 
-    def get_contact(self, name):
-        return self.contacts.get(name, None)
+    def load(self):
+        if os.path.exists(self.filename):
+            with open(self.filename, 'r') as file:
+                self.data = json.load(file)
+        else:
+            self.data = {}
 
     def save(self):
         with open(self.filename, 'w') as file:
-            json.dump(self.contacts, file, indent=4)
+            json.dump(self.data, file, indent=4)
 
-    def load(self):
-        with open(self.filename, 'r') as file:
-            self.contacts = json.load(file)
-
-# Приклад використання
-if __name__ == "__main__":
-    book = AddressBook()
-  
+class Record:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
