dbf6610f88a3ea4becdc5c425a496ad437e486a2
joseph <jozephhog@gmail.com>
2024-08-14 21:55:30 +0300

    Реалізовано методи у класі Contact

--- src/molules/contact.py
+++ src/molules/contact.py
@@ -47,4 +47,32 @@ class Birthday(Field):
 class Record:
     """"Клас для зберігання інформації про 
     контакт"""
-    pass
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+        self.birthday = None
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+    def add_phone(self, phone_number):
+        self.phones.append(Phone(phone_number))
+
+    def find_phone(self, phone_number):
+        for phone in self.phones:
+            if phone.value == phone_number:
+                return phone
+        return None
+
+    def edit_phone(self, old_phone_number, new_phone_number):
+        self.remove_phone(old_phone_number)
+        self.add_phone(new_phone_number)
+
+    def remove_phone(self, phone_number):
+        for index, p in enumerate(self.phones):
+            if p.value == phone_number:
+                del self.phones[index]
+                return
+
+    def add_birthday(self, birthday):
+        self.birthday = Birthday(birthday)
